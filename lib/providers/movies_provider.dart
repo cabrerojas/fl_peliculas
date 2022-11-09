f23a01926befc6d6b7b2c9b931e3fada55551427
Guillermo Cabrera Rojas <gcabrerojas@gmail.com>
2022-11-08 22:37:25 -0300

    subida

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,10 +1,26 @@
-class MoviesProvider {
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/models.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _apiKey = '7640bdbb799bef0b49b6ff98c5155b10';
+  final String _language = 'es-CL';
+
   MoviesProvider() {
     print('MoviesProovider inicializado');
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+    var response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    print(nowPlayingResponse.results[0].title);
   }
 }
